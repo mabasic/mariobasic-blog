712196ceb1edd0173fab8f99df1db459c021903b
Mario Bašić <mario@laravelista.hr>
2019-08-26 00:54:02 +0200

    Adds social icons for patreon and paypal. Adds laravelista logo; desktop & mobile with color changing. Adds author and sample posts from personal blog.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,91 @@
-module.exports = {
-  siteMetadata: {
-    title: `Novela by Narative`,
-    name: `Narative`,
-    siteUrl: `https://novela.narative.co`,
-    description: `This is my description that will be used in the meta tags and important for search results`,
-    hero: {
-      heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
-      maxWidth: 652,
-    },
-    social: [
-      {
-        name: `twitter`,
-        url: `https://twitter.com/narative`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/narative`,
-      },
-      {
-        name: `instagram`,
-        url: `https://instagram.com/narative.co`,
-      },
-      {
-        name: `linkedin`,
-        url: `https://www.linkedin.com/company/narative/`,
-      },
-      {
-        name: `dribbble`,
-        url: `https://dribbble.com/narativestudio`,
-      },
-    ],
+require("dotenv").config();
+
+const siteMetadata = {
+  title: `Blog by Laravelista`,
+  name: `Laravelista`,
+  siteUrl: `https://blog.laravelista.hr`,
+  description: ``,
+  hero: {
+    heading: `Welcome to Novela, the simplest way to start publishing with Gatsby.`,
+    maxWidth: 652,
   },
-  plugins: [
+  social: [
     {
-      resolve: "@narative/gatsby-theme-novela",
-      options: {
-        contentPosts: "content/posts",
-        contentAuthors: "content/authors",
-        basePath: "/",
-        authorsPage: true,
-        sources: {
-          local: true,
-          // contentful: true,
-        },
-      },
+      url: `https://github.com/laravelista`,
     },
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Novela by Narative`,
-        short_name: `Novela`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#fff`,
-        display: `standalone`,
-        icon: `src/assets/favicon.png`,
-      },
+      url: `https://www.patreon.com/laravelista`,
+    },
+    {
+      url: `https://www.paypal.me/laravelista`,
     },
   ],
+};
+
+const plugins = [
+  {
+    resolve: "@narative/gatsby-theme-novela",
+    options: {
+      contentPosts: "content/posts",
+      contentAuthors: "content/authors",
+      basePath: "/",
+      authorsPage: true,
+      mailchimp: true,
+      sources: {
+        local: true,
+        contentful: false,
+      },
+    },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Novela by Narative`,
+      short_name: `Novela`,
+      start_url: `/`,
+      background_color: `#fff`,
+      theme_color: `#fff`,
+      display: `standalone`,
+      icon: `src/assets/favicon.png`,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-118232427-3",
+    },
+  },
+  {
+    resolve: "gatsby-plugin-mailchimp",
+    options: {
+      endpoint:
+        "https://narative.us19.list-manage.com/subscribe/post?u=65ef169332a03669b9538f6ef&amp;id=c55c426282",
+    },
+  },
+];
+
+/**
+ * For development purposes if there's no Contentful Space ID and Access Token
+ * set we don't want to add in gatsby-source-contentful because it will throw
+ * an error.
+ *
+ * To enanble Contentful you must
+ * 1. Create a new Space on contentful.com
+ * 2. Import the Contentful Model from @narative/gatsby-theme-novela/conteful
+ * 3. Add .env to www/ (see www/env.example)
+ * 4. Enable contentful as a source in this file for @narative/gatsby-theme-novela
+ */
+if (process.env.CONTENTFUL_SPACE_ID && process.env.CONTENTFUL_ACCESS_TOKEN) {
+  plugins.push({
+    resolve: "gatsby-source-contentful",
+    options: {
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
+  });
+}
+
+module.exports = {
+  siteMetadata,
+  plugins,
 };
